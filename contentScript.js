0db05eb6ea0236bbd856edc8525785f4556effed
Prashant Khurana <github@prashantkhurana.com>
2023-04-06 05:09:38 +0530

    format

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,7 @@
-window.onload = function() {
+window.onload = function () {
     // Regular expression to match the keywords "AI" or "GPT"
     const keywordRegex = /\b(AI|GPT)\b/i;
-  
+
     // Find all the submissions on the page
     const submissions = document.querySelectorAll(".athing");
     const filteredLinks = [];
@@ -11,25 +11,24 @@ window.onload = function() {
     }
     // Loop through the submissions
     for (let i = 0; i < submissions.length; i++) {
-      const submission = submissions[i];
-  
-      // Find the submission title and content elements
-      const title = submission.querySelector(".title a");
-      const content = submission.nextElementSibling;
-  
-     // Check if the submission title or content contains the keywords
-      const shouldRemove = (title && keywordRegex.test(title.textContent)) || content && keywordRegex.test(content.textContent);
-      if (shouldRemove) {
-        const link = {
-            title: title ? title.textContent.trim() : "",
-            url: 'https://news.ycombinator.com/item?id=' + submission.getAttribute('id')
-        };
-        filteredLinks.push(link);
-        submission.parentNode.removeChild(submission);
-      }
+        const submission = submissions[i];
+
+        // Find the submission title and content elements
+        const title = submission.querySelector(".title a");
+        const content = submission.nextElementSibling;
+
+        // Check if the submission title or content contains the keywords
+        const shouldRemove = (title && keywordRegex.test(title.textContent)) || content && keywordRegex.test(content.textContent);
+        if (shouldRemove) {
+            const link = {
+                title: title ? title.textContent.trim() : "",
+                url: 'https://news.ycombinator.com/item?id=' + submission.getAttribute('id')
+            };
+            filteredLinks.push(link);
+            submission.parentNode.removeChild(submission);
+        }
     }
-    
+
     chrome.storage.local.set({ filteredLinks: JSON.stringify(filteredLinks) });
     chrome.runtime.sendMessage({ action: "contentScriptFinished" });
-  };
-  
+};
